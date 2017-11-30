a7eb7d62ba141c3c581c926ef083b7db53db71ed
Daniel <arutyunyan.daniel23@mail.ru>
2017-11-30 16:40:02 +0300

    ChangeWriteLine

--- WorkWithGit/Program.cs
+++ WorkWithGit/Program.cs
@@ -10,7 +10,7 @@ namespace WorkWithGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello Dany!");
         }
     }
 }
