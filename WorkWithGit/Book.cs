675526366f691e86e8d337597159c5e5b54140cc
Daniel <arutyunyan.daniel23@mail.ru>
2017-11-30 16:46:30 +0300

    CreatClassBook

---
+++ WorkWithGit/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithGit
+{
+    class Book
+    {
+        
+        public Book(string Name,string Author)
+        {
+            this._name = Name;
+            this._author = Author;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return _author;
+            }
+        }
+
+        private string _name;
+        private string _author;
+    }
+}
